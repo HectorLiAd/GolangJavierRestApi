8e057c61560757d8d3586689adb119406efa8fa7
HectorLiAd <hectorlimahuaya@upeu.edu.pe>
2020-10-16 02:46:44 -0500

    Database Connection

---
+++ database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func InitDB() *sql.DB {
+	connectionStr := "root:hector@tcp(localhost:3306)/pruebago"
+	fmt.Println(connectionStr)
+	databaseConnection, err := sql.Open("mysql", connectionStr)
+
+	if err != nil || databaseConnection == nil {
+		panic(err.Error()) //Error Handling manejo de errores
+	}
+	return databaseConnection
+}
--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"net/http"
+	"GolangJavierRestApi/database"
+	"fmt"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":3000", r)
+	databaseConnection := database.InitDB()
+
+	defer databaseConnection.Close()
+
+	fmt.Println(databaseConnection)
 }
